2e2f6730d839ca83c3a33718d5783f6bd33c8dc4
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-20 22:16:04 +1100

    Created BillPay model

---
+++ s3717205-a2/s3717205-a2/Models/BillPay.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcBank.Models;
+
+public class BillPay 
+{
+	public int BillPayID { get; set; }
+	public int AccountNumber { get; set; }
+	public int PayeeID { get; set; }
+	public decimal Amount { get; set; }
+	public DateTime ScheduleTImeUtc { get; set; }
+	public char Period { get; set; }
+}
