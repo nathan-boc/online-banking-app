6613ea614f47b87543cff2927435c29069cb7e11
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-20 22:11:48 +1100

    Created Account model

---
+++ s3717205-a2/s3717205-a2/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcBank.Models;
+
+public class Account
+{
+	// TODO : ensure this is set to the primary key
+	public int AccountNumber { get; set; }
+	public char AccountType { get; set; }
+	public int CustomerID { get; set; }
+}
