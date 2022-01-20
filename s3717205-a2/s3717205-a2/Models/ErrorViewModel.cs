c41000f9aa4fd98a1149fea55bc2fd33f127b8d9
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-20 21:40:02 +1100

    Initialised MVC project

---
+++ s3717205-a2/s3717205-a2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace s3717205_a2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
