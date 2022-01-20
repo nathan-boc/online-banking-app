066f15f9006399f7f132e4fbf0267b888562e1cf
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-20 22:07:36 +1100

    Created Customer model

---
+++ s3717205-a2/s3717205-a2/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace MvcBank.Models;
+
+public class Customer 
+{
+	public int CustomerID { get; set; }
+	public string Name { get; set; }
+	public string TFN { get; set; }
+	public string Address { get; set; }
+	public string Suburb { get; set; }
+	public string State { get; set; }
+	public string Postcode { get; set; }
+	public string Mobile { get; set; }
+}
