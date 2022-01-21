07d7d0cd487fefb585c7de6440ff4cd232266785
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-21 11:24:13 +1100

    Created Payee model

---
+++ s3717205-a2/s3717205-a2/Models/Payee.cs
@@ -0,0 +1,12 @@
+﻿namespace MvcBank.Models;
+
+public class Payee 
+{
+	public int PayeeID { get; set; }
+	public string Name { get; set; }
+	public string Address { get; set; }
+	public string Suburb { get; set; }
+	public string State { get; set; }
+	public string Postcode { get; set; }
+	public string Phone { get; set; }
+}
