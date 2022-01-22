e787142e4255b87e96fe07ee800f26dbd045b0a4
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-22 17:06:40 +1100

    Added data annotations for Account model

--- s3717205-a2/s3717205-a2/Models/Account.cs
+++ s3717205-a2/s3717205-a2/Models/Account.cs
@@ -1,11 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace MvcBank.Models;
 
 public class Account
 {
-	[Key]
+	[Key, Required]
+	[MaxLength(4), MinLength(4)]
 	public int AccountNumber { get; set; }
+
+	[Required]
+	[RegularExpression("^[CS]{1}", ErrorMessage = "Must be either 'C' for Checking or 'S' for Savings.")]
 	public char AccountType { get; set; }
+
+	[Required]
+	// TODO : Add Customer foreign key annotation here
 	public int CustomerID { get; set; }
 }
