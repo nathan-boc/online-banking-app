2a10e6896e502060205aa7077361dbfe50f1d09c
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-22 22:13:47 +1100

    Added data annotations for Login model

--- s3717205-a2/s3717205-a2/Models/Login.cs
+++ s3717205-a2/s3717205-a2/Models/Login.cs
@@ -1,8 +1,18 @@
-﻿namespace MvcBank.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcBank.Models;
 
 public class Login 
 {
+	[Required]
+	[MaxLength(8), MinLength(8)]
 	public string LoginID { get; set; }
+
+	[Required]
+	// TODO : Add Customer foreign key annotation here
 	public int CustomerID { get; set; }
+
+	[Required]
+	[MaxLength(64)]
 	public string PasswordHash { get; set; }
 }
