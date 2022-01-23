332ce9e771dd77d1bb445c696cc6546fab003003
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-23 14:02:24 +1100

    Added data annotations for BillPay model

--- s3717205-a2/s3717205-a2/Models/BillPay.cs
+++ s3717205-a2/s3717205-a2/Models/BillPay.cs
@@ -1,11 +1,28 @@
-﻿namespace MvcBank.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcBank.Models;
 
 public class BillPay 
 {
+	[Required]
 	public int BillPayID { get; set; }
+
+	[Required]
+	// TODO : Add Account foreign key annotation here
 	public int AccountNumber { get; set; }
+
+	[Required]
+	// TODO : Add Payee foreign key annotation here
 	public int PayeeID { get; set; }
+
+	[Required]
+	[DataType(DataType.Currency), Range(0.0, Double.MaxValue)]
 	public decimal Amount { get; set; }
+
+	[Required]
 	public DateTime ScheduleTImeUtc { get; set; }
+
+	[Required]
+	[RegularExpression("^[OM]{1}", ErrorMessage = "Must be either 'O' for One-off or 'M' for Monthly.")]
 	public char Period { get; set; }
 }
