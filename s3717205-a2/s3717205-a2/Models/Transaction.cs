748ccca8d2966445fd1253d30bf4152c4ab267bb
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-23 13:16:42 +1100

    Added data annotations for Transaction model

--- s3717205-a2/s3717205-a2/Models/Transaction.cs
+++ s3717205-a2/s3717205-a2/Models/Transaction.cs
@@ -1,12 +1,30 @@
-﻿namespace MvcBank.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcBank.Models;
 
 public class Transaction 
 {
+	[Required]
 	public int TransactionID { get; set; }
+
+	[Required]
+	[RegularExpression("^[DWTSB]{1}", ErrorMessage = "Must be a valid transaction type (D/W/T/S/B).")]
 	public char TransactionType { get; set; }
+
+	[Required]
+	// TODO : Add Account foreign key annotation here
 	public int AccountNumber { get; set; }
+
+	// TODO : Add Account foreign key annotation here
 	public int DestinationAccountNumber { get; set; }
+
+	[Required]
+	[DataType(DataType.Currency), Range(0.0, Double.MaxValue)]
 	public decimal Amount { get; set; }
+
+	[MaxLength(30)]
 	public string Comment { get; set; }
+
+	[Required]
 	public DateTime TransactionTimeUtc { get; set; }
 }
