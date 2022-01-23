e868ab54b0765bd5db3b846ebee0699489ea27b9
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-23 14:10:42 +1100

    Added data annotations for Payee model

--- s3717205-a2/s3717205-a2/Models/Payee.cs
+++ s3717205-a2/s3717205-a2/Models/Payee.cs
@@ -1,12 +1,35 @@
-﻿namespace MvcBank.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class Payee 
+namespace MvcBank.Models;
+
+public class Payee
 {
+	[Required]
 	public int PayeeID { get; set; }
+
+	[Required]
+	[MaxLength(50)]
 	public string Name { get; set; }
+
+	[Required]
+	[MaxLength(50)]
 	public string Address { get; set; }
+
+	[Required]
+	[MaxLength(40)]
 	public string Suburb { get; set; }
+
+	[Required]
+	[AustralianState]
 	public string State { get; set; }
+
+	[Required]
+	// Regex to match postcodes 0000 - 9999
+	[RegularExpression(@"^[0-9]{4}$", ErrorMessage = "Must be a valid postcode between 0000 - 9999.")]
 	public string Postcode { get; set; }
+
+	[Required]
+	// Regex to match phone number format '(0X) XXXX XXXX'
+	[RegularExpression(@"^(0[0-9]{1}) [0-9]{4} [0-9]{4}$", ErrorMessage = "Must be in the format '(0X) XXXX XXXX'.")]
 	public string Phone { get; set; }
 }
