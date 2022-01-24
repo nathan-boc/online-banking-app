a3bedb83886973a8a12ca10fedeae1bd92bf2476
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-25 07:47:37 +1100

    Added basic Customer controller and view files

---
+++ s3717205-a2/s3717205-a2/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace s3717205_a2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: HomeController1
+        public ActionResult Index() => View();
+        
+
+        // GET: HomeController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HomeController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
