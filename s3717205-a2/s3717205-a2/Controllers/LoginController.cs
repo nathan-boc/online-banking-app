bc64b682c421afbe14dec84c13845e9ba57ed2d1
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-25 08:27:26 +1100

    Added logic for user login in LoginController

--- s3717205-a2/s3717205-a2/Controllers/LoginController.cs
+++ s3717205-a2/s3717205-a2/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcBank.Data;
+using MvcBank.Models;
+using SimpleHashing;
 
 namespace s3717205_a2.Controllers
 {
@@ -10,5 +12,26 @@ namespace s3717205_a2.Controllers
         public LoginController(MvcBankContext context) => _context = context;
 
         public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string loginID, string password)
+        {
+            // Retrieve Login object from database using the given LoginID
+            var login = await _context.Login.FindAsync(loginID);
+
+            // Checks if LoginID or password fields are empty or if password matches hashed value from database
+            if(login == null || string.IsNullOrEmpty(password) || PBKDF2.Verify(login.PasswordHash, password) == false)
+            {
+                ModelState.AddModelError("LoginFailed", "Invalid username or password, please try again.");
+                return View(new Login { LoginID = loginID });
+            }
+
+            // Login customer by adding details into session data
+            HttpContext.Session.SetInt32(nameof(Customer.CustomerID), login.CustomerID);
+            HttpContext.Session.SetString(nameof(Customer.Name), login.Customer.Name);
+
+            // Successful login passes user to the index page of CustomerController
+            return RedirectToAction("Index", "Customer");
+        }
     }
 }
