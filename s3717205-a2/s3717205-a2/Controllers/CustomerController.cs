df5f7c298f105bfc4c79c3e69e1eb83ad3391d82
Nathan12313 <nathan.m.boc@gmail.com>
2022-01-29 23:47:52 +1100

    Fixed ordering of transactions to be descending by transaction time

--- s3717205-a2/s3717205-a2/Controllers/CustomerController.cs
+++ s3717205-a2/s3717205-a2/Controllers/CustomerController.cs
@@ -98,7 +98,7 @@ namespace s3717205_a2.Controllers
                 ViewBag.Account = account;
 
                 // Retrieves 4 transactions at a time matching account number
-                var transactionList = await _context.Transaction.Where(x => x.AccountNumber == accountNumber).OrderBy(x => x.TransactionTimeUtc)
+                var transactionList = await _context.Transaction.Where(x => x.AccountNumber == accountNumber).OrderByDescending(x => x.TransactionTimeUtc)
                     .Skip(pageSize * page).Take(pageSize).ToListAsync();
 
                 // Throw error when going out of bounds
